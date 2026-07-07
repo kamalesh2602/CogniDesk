9030831ec9c54383af8d09cd8a0be84555e2107a
kamalesh2602 <kamaleshg2602@gmail.com>
2026-07-07 22:19:34 +0530

    main -1

--- server/main.py
+++ server/main.py
@@ -1,9 +1,23 @@
 from fastapi import FastAPI
 
-print("HELLO FROM MAIN")
-
 app = FastAPI()
+
+print("MAIN STARTED")
+
+from api.auth import router as auth_router
+print("✓ auth")
+
+from api.workspaces import router as workspace_router
+print("✓ workspaces")
+
+from api.documents import router as document_router
+print("✓ documents")
+
+app.include_router(auth_router)
+app.include_router(workspace_router)
+app.include_router(document_router)
+
 
 @app.get("/")
 def root():
-    return {"status": "ok"}
+    return {"status": "OK"}
